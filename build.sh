5be9de11dab8f6c81ee8089ce752749ab028df30
Matt Lyons <lyonzy@fastmail.com>
2019-10-03 07:46:37 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ build_firmware() {
   source build/envsetup.sh
   lunch lineage_hammerhead-userdebug
   #mka bacon
-  timeout 2h make -j32 bacon
+  timeout 10m make -j32 bacon || true
   retVal=$?
   if [ $retVal -eq 124 ]; then
     echo "Timed out, saving cache"
